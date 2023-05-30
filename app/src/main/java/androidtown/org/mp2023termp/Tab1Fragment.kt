01e60832d1937268c986fdad43c5df2261f9aa66
ahyoung <ahyoung120613@gmail.com>
2023-05-30 18:54:00 +0900

    단어장 첫 번째 단어 파이어베이스 실시간 데이터베이스 값 읽어오기

--- app/src/main/java/androidtown/org/mp2023termp/Tab1Fragment.kt
+++ app/src/main/java/androidtown/org/mp2023termp/Tab1Fragment.kt
@@ -1,24 +1,63 @@
 package androidtown.org.mp2023termp
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidtown.org.mp2023termp.databinding.FragmentTab1Binding
+import com.google.firebase.database.*
+import com.google.firebase.database.FirebaseDatabase
 
 class Tab1Fragment : Fragment() {
     private var _binding: FragmentTab1Binding? = null
     private val binding get() = _binding!!
 
+    private lateinit var database: DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        database = FirebaseDatabase.getInstance().reference
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?, ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         _binding = FragmentTab1Binding.inflate(inflater, container, false)
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        readDataFromFirebase()
+    }
+
+    private fun readDataFromFirebase() {
+        val termTextView = binding.firstWordTerm
+        val meaningTextView = binding.firstWordMeaning
+
+        database.child("word").child("1").addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    val termValue = dataSnapshot.child("Term").getValue(String::class.java)
+                    val meaningValue = dataSnapshot.child("Meaning").getValue(String::class.java)
+
+                    termTextView.text = termValue
+                    meaningTextView.text = meaningValue
+                } else {
+                    Log.d(TAG, "Data does not exist in Firebase")
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.e(TAG, "Error retrieving data from Firebase: ${error.message}")
+            }
+        })
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
 
